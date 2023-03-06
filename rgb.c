204c98187876ef8579242c3631536275fe456638
Tobias Walle <tobias.walle@maibornwolff.de>
2023-03-06 22:54:51 +0100

    tweak colors

--- rgb.c
+++ rgb.c
@@ -7,45 +7,47 @@ void rgb_matrix_sethsv_at(uint8_t led, uint8_t h, uint8_t s, uint8_t v) {
   rgb_matrix_set_color(led, rgb.r, rgb.g, rgb.b);
 }
 
-void set_board_colors_side(uint8_t offset, uint8_t h, uint8_t s, uint8_t v) {
+void set_board_colors_side(uint8_t offset, uint8_t hue, uint8_t hue_backglow) {
+  int brightness = 50;
+
+  // Keys
+  int saturation = 180;
   for(int i = 0; i < 24; i++) {
     uint8_t led = i + offset + 6;
     if (led > 0) {
-      uint8_t x = i / 4 * 10;
-      rgb_matrix_sethsv_at(led, h + x, s, v);
+      uint8_t x = i / 4 * 7;
+      rgb_matrix_sethsv_at(led, hue + x, saturation, brightness);
     }
   }
-  // Backlight
+
+  // Backglow
   for(int i = 0; i < 6; i++) {
     uint8_t led = i + offset;
-      rgb_matrix_sethsv_at(led, h - 100, 255, 180);
+      rgb_matrix_sethsv_at(led, hue_backglow, 255, brightness + 120);
   }
 }
 
-void set_board_colors(uint8_t h, uint8_t s, uint8_t v) {
+void set_board_colors(uint8_t hue, uint8_t hue_backglow) {
   // Left
-  set_board_colors_side(0, h, s, v);
+  set_board_colors_side(0, hue, hue_backglow);
   // Right
-  set_board_colors_side(27, h, s, v);
+  set_board_colors_side(27, hue, hue_backglow);
 }
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    int saturation = 180;
-    int brightness = 50;
     switch(get_highest_layer(layer_state|default_layer_state)) {
-        case _BASE:
-            set_board_colors(100, saturation, brightness);
-            break;
         case _LOWER:
-            set_board_colors(50, saturation, brightness);
+            set_board_colors(70, 180);
+            break;
+        case _BASE:
+            set_board_colors(100, 15);
             break;
         case _RAISE:
-            set_board_colors(150, saturation, brightness);
+            set_board_colors(150, 50);
             break;
         case _META:
             rgb_matrix_sethsv(0, 0, 10);
             break;
     };
-    /* rgb_matrix_sethsv_at(5, 0, saturation, 255); */
     return false;
 }
